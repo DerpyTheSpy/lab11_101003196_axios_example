aa7cd83e96436845f68a3a9be26bf94a5dc5aafe
DerpyTheSpy <erickim334@gmail.com>
2022-11-17 14:13:30 -0500

    Finishing things up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from './PersonList';
+
+function App() {
+  return (
+    <>
+      <h1>Lab11_101003196</h1>
+      <div>
+        <h1 style={{backgroundColor: "#50C878", textAlign: 'center'}}>User List</h1>
+      </div>
+      <PersonList/>
+    </>
+  );
+}
+
+export default App;
